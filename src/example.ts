63b3e36b468ef31949be9532abb89a5e9fb38419
Andrey Simonov <sasmeo@gmail.com>
2021-05-14 17:38:48 +0800

    fix2

--- src/example.ts
+++ src/example.ts
@@ -1,5 +1,28 @@
-function concat (str1:string, str2:string){
+function concat (str1:string, str2:string): string{
     return `${str1} ${str2}`;
 }
 
 concat('Hello ', 'World');
+
+interface TsMyHometask { 
+    howIDoIt: string, 
+    simeArray: any, 
+    withData?: any  
+};
+const myHometask: TsMyHometask = {
+	howIDoIt: "I Do It Wel",
+	simeArray: ["string one", "string two", 42],
+	withData: [{ howIDoIt: "I Do It Wel", simeArray: ["string one", 23] }],
+}
+
+myHometask.howIDoIt = 'eslint rygaets9';  
+
+interface MyArray<T> {
+	[N: number]: T;
+
+	reduce(fn: (el: T, em: T) => T): T[];
+}
+
+
+const tsArr: MyArray<number> = []; 
+tsArr.reduce((acc: number, cur: number) => acc + cur);
