2c112dbc60eb56240b334f0e228e5d54c325663b
Andrey Simonov <sasmeo@gmail.com>
2021-05-20 11:50:46 +0800

    fix5

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -1,6 +1,5 @@
 import React, { FC } from 'react';
 
-import s from './Heading.module.scss';
 
 interface IProps {
   tag: 'p' | 'h6' | 'h5' | 'h4' | 'h3' | 'h2' | 'h1';
@@ -8,7 +7,7 @@ interface IProps {
 
 const Heading: FC<IProps> = ({ tag = 'p', children }) => {
   const Tag = tag as keyof JSX.IntrinsicElements
-  return <Tag>{children}</Tag>
+  return <Tag className='root'>{children}</Tag>
 };
 
 export default Heading;
