680192ac360464326c7322b314b4559d91c3f9a3
Andrey Simonov <sasmeo@gmail.com>
2021-05-31 07:23:28 +0800

    fix2

--- src/utils/getUrlWithParamsConfig.ts
+++ src/utils/getUrlWithParamsConfig.ts
@@ -5,7 +5,7 @@ type TEndpoint = keyof typeof config.client.endpoint;
 const getUrlWithParamsConfig = (endpointConfig: TEndpoint) => {
   const url = {
     ...config.client.server,
-    ...config.client.endpoint[endpointConfig].uri,
+    ...config.client.endpoint[endpointConfig as keyof typeof config.client.endpoint].uri,
   };
 
   return url;
