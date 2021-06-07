885bba01a7cd08069dc4f7d5b58c3d9df8e5c4a3
Andrey Simonov <sasmeo@gmail.com>
2021-06-07 14:01:57 +0800

    last fix 2

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,12 @@
-'use strict';
-
-const Hapi = require('@hapi/hapi');
+import fs from 'fs';
+import path from 'path';
+import handlebars from 'handlebars';
+import Hapi from '@hapi/hapi';
+import React from 'react';
+import ReactDom from 'react-dom/server';
+import {setPath} from 'hookrouter';
+import App from '../App';
+import NotFoundPage from '../pages/NotFound';
 
 const init = async () => {
 
@@ -9,12 +15,26 @@ const init = async () => {
         host: 'localhost'
     });
 
+    await server.register(require('@hapi/inert'));
+
+    server.route({
+        method: 'GET',
+        path: '/main.js',
+        handler: (request, h) => h.file(path.join(process.cwd(), 'dist', 'main.js'))
+    });
+
     server.route({
         method: 'GET',
         path: '/{any*}',
-        handler: (request, h) => {
-
-            return 'Hello World!';
+        handler: (request) => {
+            setPath(request.path);
+            const pathIndexHTML = path.join(process.cwd(), 'dist', 'index.html');
+            const template = handlebars.compile(fs.readFileSync(pathIndexHTML, 'utf8'));
+            const result = ReactDom.renderToNodeStream(<App />);
+            const page = template({
+                content: result,
+            });
+            return page;
         }
     });
 
